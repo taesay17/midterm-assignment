90c97b5f94c900df0e525e7226406f3e107e5681
taesay17 <151002316+taesay17@users.noreply.github.com>
2024-03-26 17:13:55 +0600

    changing

--- js/resume.js
+++ js/resume.js
@@ -1,33 +1,33 @@
 
-var select = function(s) {
-    return document.querySelector(s);
-  }
+// var select = function(s) {
+//     return document.querySelector(s);
+//   }
   
-  function randomBetween(min,max)
-  {
-      var number = Math.floor(Math.random()*(max-min+1)+min);
+//   function randomBetween(min,max)
+//   {
+//       var number = Math.floor(Math.random()*(max-min+1)+min);
     
-      if ( number !== 0 ){
-        return number;
-      }else {
-        return 0.5;
-      }
-  }
+//       if ( number !== 0 ){
+//         return number;
+//       }else {
+//         return 0.5;
+//       }
+//   }
   
-  var tl = new TimelineMax();
+//   var tl = new TimelineMax();
   
-  for(var i = 0; i < 20; i++){
+//   for(var i = 0; i < 20; i++){
   
-    var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-      x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-      y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
-      repeat:-1,
-      repeatDelay:randomBetween(0.2, 0.5),
-      yoyo:true,
-      ease:Elastic.easeOut.config(1, 0.5)
-    })
+//     var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
+//       x: randomBetween(12, 15) * (randomBetween(-1, 1)),
+//       y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
+//       repeat:-1,
+//       repeatDelay:randomBetween(0.2, 0.5),
+//       yoyo:true,
+//       ease:Elastic.easeOut.config(1, 0.5)
+//     })
   
-    tl.add(t, (i+1)/0.6)
-  }
+//     tl.add(t, (i+1)/0.6)
+//   }
   
-  tl.seek(50);
+//   tl.seek(50);
